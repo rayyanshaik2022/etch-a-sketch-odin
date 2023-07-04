41ce6d2572edb176c1e97308b1c290dcdb88383d
Rayyan Shaik <rayyanshaik2022@gmail.com>
2023-07-03 23:43:18 -0500

    Added drawing functionality

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ function createGrid(size) {
 
     for (let i = 0; i < size; i++) {
         let nextRow = document.createElement("div");
-        nextRow.setAttribute("class", 'row');
+        nextRow.setAttribute("class", "row");
         container.appendChild(nextRow);
 
         for (let j = 0; j < size; j++) {
             let nextDivColItem = document.createElement("div");
-            nextDivColItem.setAttribute("class", 'grid-box');
+            nextDivColItem.setAttribute("class", "grid-box");
 
             nextRow.appendChild(nextDivColItem);
         }
     }
 }
 
-createGrid(40);
+createGrid(20);
+
+// Color tiles black on hover
+boxes = document.querySelectorAll(".grid-box")
+boxes.forEach( (box) => {
+   box.addEventListener("mouseover", (e) => {
+        box.style['background-color'] = 'black';
+        console.log('test');
+   }); 
+});
+
+
+
